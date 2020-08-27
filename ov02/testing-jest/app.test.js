ed82f9418f5e06dca6c7f9cc5ef48d3195837980
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-08-27 09:30:14 +0200

    Completed assignment

--- ov02/testing-jest/app.test.js
+++ ov02/testing-jest/app.test.js
@@ -1,8 +1,9 @@
 import { isLeapYear } from './app.js';
 
 describe('A year is a leap year', () => {
-    test('Year is divisible by 4 but not by 100', () => {
-        expect(isLeapYear(2020)).toBeTruthy();
+    // Oppgave 1
+    test.each([2020, 1820, 1960, 2020])('Year is divisible by 4 but not by 100', (year) => {
+        expect(isLeapYear(year)).toBeTruthy();
     });
     
     test('Year is divisible by 400', () => {
@@ -17,5 +18,27 @@ describe('A year is not a leap year', () => {
 
     test('Year is divisible by 100 but not by 400', () => {
        expect(isLeapYear(2100)).toBeFalsy(); 
+    });
+});
+
+describe('A year is not supported', () => {
+    test('Year is negative', () => {
+        // When expecting a function to throw an error, 
+        // we need an anonymous wrapper function
+        expect(() => {
+            isLeapYear(-1);
+        }).toThrow('Invalid argument: year must be an integer equal to or larger than 0');
+    });
+    
+    test('Year is null', () => {
+        expect(() => {
+            isLeapYear(null);
+        }).toThrow('Invalid argument: year can not be null or undefined');
+    });
+
+    test('Year is undefined', () => {
+        expect(() => {
+            isLeapYear(undefined);
+        }).toThrow('Invalid argument: year can not be null or undefined');
     });
 });

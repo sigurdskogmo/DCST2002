757c6b195f69e2af77c994fe3faf22c36f845520
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-08-30 21:54:17 +0200

    Setup

---
+++ ov03/todo-api/data.js
@@ -0,0 +1,19 @@
+const tasks = [
+    {
+        id: 1,
+        title: "Les leksjon",
+        done: false
+    },
+    {
+        id: 2,
+        title: "Møt opp på forelesning",
+        done: false
+    },
+    {
+        id: 3,
+        title: "Gjør øving",
+        done: false
+    }
+];
+
+export default tasks;

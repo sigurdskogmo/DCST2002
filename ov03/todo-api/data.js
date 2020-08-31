963da3c4afff3c24651587eda3b47a9520de076f
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-08-31 11:17:55 +0200

    Create new API that handles lists with tasks

--- ov03/todo-api/data.js
+++ ov03/todo-api/data.js
@@ -1,4 +1,52 @@
-const tasks = [
+const lists = [
+    {
+        listId: 1,
+        listTitle: "Webutvikling",
+        tasks: [
+            {
+                id: 1,
+                title: "Les leksjon",
+                done: false
+            },
+            {
+                id: 2,
+                title: "Møt opp på forelesning",
+                done: false
+            },
+            {
+                id: 3,
+                title: "Gjør øving",
+                done: false
+            }
+        ]
+    },
+    {
+        listId: 2,
+        listTitle: "Ex.phil",
+        tasks: [
+            {
+                id: 1,
+                title: "Les pensum",
+                done: false
+            },
+            {
+                id: 2,
+                title: "Se videoforelesning",
+                done: false
+            },
+            {
+                id: 3,
+                title: "Klø seg i hodet",
+                done: true
+            }
+        ]
+    }
+];
+
+export default lists;
+
+// Data to be used in oldApp.js
+/* const tasks = [
     {
         id: 1,
         title: "Les leksjon",
@@ -16,4 +64,4 @@ const tasks = [
     }
 ];
 
-export default tasks;
+export default tasks; */

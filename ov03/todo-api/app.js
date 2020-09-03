7dc04f8beb5f37b70585a7535696b6c2d2c057a1
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-09-03 08:51:17 +0200

    Cleanup app.js

--- ov03/todo-api/app.js
+++ ov03/todo-api/app.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import lists from './data';
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-const router = express.Router();
 app.use(routes);
-
 
 const PORT = 3000;
 app.listen(PORT, () => {
